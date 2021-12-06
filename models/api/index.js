0ad8fe6bdbb09e3cb69ef95a8dc1abdda865fbbf
Tristanoshier <tsoshier@bsu.edu>
2021-12-06 17:09:00 -0500

    add recipe model

--- models/api/index.js
+++ models/api/index.js
@@ -24,9 +24,37 @@ const Profile = db.define('profile', {
   }
 });
 
+const Recipe = db.define('recipe', {
+  name: {
+    type: DataTypes.STRING,
+    defaultValue: 'Spaghetti'
+  },
+  directions: {
+    type: DataTypes.STRING,
+    defaultValue: 'We add a lot of salt to our pasta water (it makes the pasta taste delicious). For every 4 quarts (16 cups) of water, we add about one tablespoon of salt. This is perfect for cooking 1 pound of pasta.'
+  },
+  cookTime: {
+    type: DataTypes.STRING,
+    defaultValue: '20 Minutes'
+  },
+  servingSize: {
+    type: DataTypes.INTEGER,
+    defaultValue: 2
+  },
+  photoUrl: {
+    type: DataTypes.STRING,
+    defaultValue: 'https://unsplash.com/photos/PLyJqEJVre0'
+  },
+  category: {
+    type: DataTypes.STRING,
+    defaultValue: 'Breakfast'
+  },
+})
+
 // Put all other models below here for the app
 
 module.exports = {
   User,
-  Profile
+  Profile,
+  Recipe
 };
